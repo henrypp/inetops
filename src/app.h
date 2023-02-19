0ebc5863fb6adf23114013354637a5e772f8ec1b
henrypp <support@henrypp.org>
2023-02-19 21:44:09 +0600

    add app.h

---
+++ src/app.h
@@ -0,0 +1,11 @@
+// InetOps
+// Copyright (c) 2012-2023 Henry++
+
+#pragma once
+
+#define APP_NAME L"InetOps"
+#define APP_NAME_SHORT L"inetops"
+#define APP_VERSION L"1.1"
+#define APP_VERSION_RES 1,1,0,0
+#define APP_AUTHOR L"Henry++"
+#define APP_COPYRIGHT L"(c) 2012-2023 " APP_AUTHOR L". All Rights Reversed."
