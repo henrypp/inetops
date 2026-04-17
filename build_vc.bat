28cc368685725dcde3aa2349664f0b4f5a911c7b
henrypp <sforce5@mail.ru>
2026-04-17 16:19:37 +0500

    fuck off 32-bit

--- build_vc.bat
+++ build_vc.bat
@@ -20,9 +20,6 @@ goto end
 
 :start
 
-msbuild inetops.sln -property:Configuration=Release -property:Platform=Win32 -verbosity:normal
-if %ERRORLEVEL% neq 0 goto end
-
 msbuild inetops.sln -property:Configuration=Release -property:Platform=x64 -verbosity:normal
 if %ERRORLEVEL% neq 0 goto end
 
